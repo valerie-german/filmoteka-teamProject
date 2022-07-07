d508475c37fdd9b015bed4fc203960cf412444a1
Valeria German <94998541+valerie-german@users.noreply.github.com>
2022-07-07 23:14:58 +0300

    adds refs in js

---
+++ src/js/refs.js
@@ -0,0 +1,8 @@
+export const refs = {
+  form: document.querySelector('.js-search-form'),    
+  gallery: document.querySelector('.gallery-home-list'),
+  modalWindow: document.querySelector('.backdrop'),
+  watched: document.querySelector('[data-action="watched"]'),
+  queue: document.querySelector('[data-action="queue"]'),
+  notification: document.querySelector('.notification'),
+};
