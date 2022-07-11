02924599a2d193ac56d61dbd562e478925e418a8
allx-under <95172339+allx-under@users.noreply.github.com>
2022-07-11 13:04:27 +0300

    add preloader and fix notify

--- src/js/refs.js
+++ src/js/refs.js
@@ -8,4 +8,10 @@ export const refs = {
   notification: document.querySelector('.notification'),
   pagination: document.querySelector('.gallery__pagination'),
   preloader: document.querySelector('#preloader'),
+  openModalFooter: document.querySelector('.footer-link'),
+    closeModalBtnFooter: document.querySelector('.team-modal__close'),
+    backdropFooter: document.querySelector('.team-modal'),
+    galleryLink: document.querySelectorAll('.gallery-home'),
+    btnModal: document.querySelector('.modal-close'),
+    backdrop: document.querySelector('.backdrop')
 };
